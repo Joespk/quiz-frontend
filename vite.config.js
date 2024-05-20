20a631baf161a36501baa52e6e6578dd6ca6d84c
joespk <supakritjoe@gmail.com>
2024-05-20 16:03:00 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "./", // ถ้าโปรเจคของคุณจะถูก deploy ใน subdirectory ให้ตั้งค่า base ให้ถูกต้อง
 });
