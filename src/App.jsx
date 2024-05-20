2df12fa439e629356af00f317a76f0ad7dffaee1
joespk <supakritjoe@gmail.com>
2024-05-20 15:28:28 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import PlayerApp from './PlayerApp'
-import AdminApp from './AdminApp'
-import Summaryplayer from './Summaryplayer'
-
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import PlayerApp from "./PlayerApp";
+import AdminApp from "./AdminApp";
+import Summaryplayer from "./Summaryplayer";
 
 function App() {
   return (
     <Routes>
-        <Route path="/" exact element={<PlayerApp/>} />
-        <Route path="/admin" element={<AdminApp />} />
-        <Route path="/summary" element={<Summaryplayer/>} />
+      <Route path="/" exact element={<PlayerApp />} />
+      <Route path="/admin" element={<AdminApp />} />
+      <Route path="/summary" element={<Summaryplayer />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
